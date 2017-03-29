64807cedaf954b84ec294ac13acead6bdb2ee010
Marko Manninen <elonmedia@gmail.com>
2017-03-29 22:33:17 +0300

    using separate config file

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "kJj5p9tth9Q824VbxJbJsYVrDebJyhEE"
+    DATABASE_URI = 'sqlite://:memory:'
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
--- shim.py
+++ shim.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import hy  # NOQA
 from app import app
 
 if __name__ == '__main__':
-    app.debug = True
-    app.secret_key = "kJj5p9tth9Q824VbxJbJsYVrDebJyhEE"
+    app.config.from_object('config.DevelopmentConfig')
     app.run()
