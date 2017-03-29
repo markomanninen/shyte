145bd1e08c1e928ded9111d26bfd59a6c98dc9c0
Marko Manninen <elonmedia@gmail.com>
2017-03-29 19:46:05 +0300

    secret and debug different way

--- shim.py
+++ shim.py
@@ -2,4 +2,6 @@ import hy  # NOQA
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.secret_key = "kJj5p9tth9Q824VbxJbJsYVrDebJyhEE"
+    app.run()
